c87474ed2c74ddd0a1d5ecf98de88f707d02fa95
Jeremy Lee <junzhli@gmail.com>
2016-10-25 12:59:21 +0800

    Feature development: OpenVPN Config files generator      - [File] Generator created

--- purevpn_server_list/pipelines.py
+++ purevpn_server_list/pipelines.py
@@ -6,6 +6,19 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+def cleanup():
+    import os
+    if os.path.isfile('./templates/data.json'):
+        try:
+            os.remove('./templates/data.json')
+        except IOError:
+            pass
+
 class PurevpnServerListPipeline(object):
+    def open_spider(self, spider):
+        cleanup()
+
     def process_item(self, item, spider):
+        item.update({key: value[0] for key, value in item.iteritems()})
+        item.update({key: value.upper() for key, value in item.iteritems() if key in ['region', 'country']})
         return item
